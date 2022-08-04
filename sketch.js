86aec5cd7e0db4296bf1745f51106c9f7c1536b1
Fahmi Ali Putra <44913598+fahmialiputra@users.noreply.github.com>
2022-08-04 17:49:56 +0700

    cleaning code

--- sketch.js
+++ sketch.js
@@ -2,9 +2,8 @@ const boids = [];
 const obstacles = [];
 const num = 15;
 let aveY = 0;
-// let newposCount = 0;
-// let t = 0;
 // let imgnum = 1;
+// let t = 0;
 
 function setup() {
     createCanvas(600, 400);
@@ -14,16 +13,13 @@ function setup() {
             // let newpos = createVector(random(width/16, width/8), random(height*(3/4), height*(3/4) + 50));
             // boids[i].position.mult(0);
             // boids[i].position.add(newpos);
-            // newposCount++;
         // }
         aveY += boids[i].position.y;
     }
-    // print('Changing boids position: ' + newposCount + 'x');
     obstacles.push(new Obstacle(width*(5/8), aveY/num, 10, 150));
     obstacles.push(new Obstacle(width*(5/8) + 50, aveY/num + 30, 10, 150));
     obstacles.push(new Obstacle(width*(5/8) + 50, aveY/num - 30, 10, 150));
     obstacles.push(new Obstacle(width*(5/8) + 120, aveY/num, 10, 150));
-    // background(50);
 }
 
 function draw() {
@@ -40,35 +36,14 @@ function draw() {
     for (let obs of obstacles) {
         obs.show();
     }
-    // boids[0].check(obstacles);
-    // boids[0].update(mouseX, mouseY);
-    // boids[0].show();
-    // push();
-    //     strokeWeight(1);
-    //     stroke(255,0,0);
-    //     line(boids[0].position.x, boids[0].position.y, obstacles[0].position.x, obstacles[0].position.y);
-    // pop();
     for (let boid of boids) {
-        // if( boid != boids[0] ){
-        //     push();
-        //         strokeWeight(1);
-        //         stroke(0,255,0);
-        //         line(boids[0].position.x, boids[0].position.y, boid.position.x, boid.position.y);
-        //     pop();
-        // }
-        // boid.overlap(boids);
         boid.update(boids,boid.check(obstacles));
         boid.show();
         boid.change = false;
     }
     // noLoop();
-    // print('Boid-1 coherency: ' + boids[0].coherency(boids));
-    // if (t > 0) {
     if (boidlast.position.x > width + boidlast.lij + 5) {
         print('Process terminated');
-        // print('Boid-1 v(' + boids[0].velocity.x + ', ' + boids[0].velocity.y +
-        // ') heading to ' + degrees(boids[0].or).toFixed(2));// boids[0].heading.x + ', ' + boids[0].heading.y + ')');
-        // t++;
         noLoop();
     }
     // } else {
@@ -76,9 +51,7 @@ function draw() {
     // }
 }
 
-function savingimg() {
-    // if (t < 300) {
-    // if (t < 300 &&  t % 2 < 1) {
+function savingimg() { // saving img per frame
         let numimg = 1;
         if(imgnum.toString().length < 3) {
             if(imgnum.toString().length < 2) {
@@ -91,6 +64,4 @@ function savingimg() {
         }
         saveCanvas('Flocking' + numimg, 'png');
         imgnum++;
-    // }
-    // t++;
 }
