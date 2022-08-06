32f67f0a5713bb21858dd8ca06fc9975e4ba2073
Fahmi Ali Putra <fahmialiputra@gmail.com>
2022-08-07 01:25:39 +0700

    code breakdown to figure out how forces are work

--- boid.js
+++ boid.js
@@ -1,41 +1,69 @@
 class Boid {
     constructor(index) {
         this.position = createVector(random(width/16, width/8), random(height*(3/4), height*(3/4) + 50));
-        this.size = 5;
+        this.size = 10;
         this.heading = createVector();
-        this.sensing = 20; // sensing radius
+        this.sensing = 50; // sensing radius
         this.velocity = createVector();
         // this.acceleration = createVector();
-        this.maxSpeed = 2;
+        this.Forcelimit = 10;
+        this.maxSpeed = 3;
         this.v0 = 0.5;
         this.alpha = 0.1;
         this.betha = 1.2;
-        this.k = 3;
+        this.k = 2;
         this.lij = 30;
         this.Dr = 0.15;
         this.Dt = 0.25;
         this.unitvector = createVector(1,0);
         this.unitvector_perpendicular = createVector(0,1);
+        this.vd = createVector(1,0);
         this.wg = 4;
-        this.Er = createVector(random(0,0.1),0);
+        this.Er = createVector(random(0,0.1),random(0,0.1));
         this.Et = random(-PI/12,PI/12);
         this.change = false;
         this.index = index;
+        this.color = color(255,255,255);
     }
 
     rij(i,j) {
         return p5.Vector.sub(j, i);
     }
 
+    rF(i,j) {
+        let r = i.rij(i.position, j.position);
+        let exceed1 = createVector(j.radius*cos(r.heading()), j.radius*sin(r.heading()));
+        r.sub(exceed1);
+        let exceed2 = createVector(i.radius*cos(r.heading()), i.radius*sin(r.heading()));
+        r.sub(exceed2);
+        return r;
+    }
+
+    equilibrium(boids) {
+        let r = createVector();
+        for (let other of boids) {
+            r.add(this.rij(this.position, other.position));
+        }
+        r.div(boids.length);
+        // r.normalize();
+        // r.mult(10);
+        // push();
+        //     strokeWeight(1);
+        //     stroke(0,255,0);
+        //     line(this.position.x, this.position.y, this.position.x + r.x, this.position.y + r.y);
+        // pop();
+        return r.mag();
+    }
+
     Fi(boids) {
         let F = createVector();
-        stroke(100);
+        // stroke(100);
         for (let other of boids) {
             if (other != this) {
                 let r = this.rij(this.position, other.position);
                 let newr = createVector(other.size*cos(r.heading()), other.size*sin(r.heading()));
                 r.sub(newr);
-                // line(this.position.x, this.position.y, other.position.x, other.position.y); // Force orientation
+                // line(this.position.x, this.position.y, other.position.x, other.position.y);
                 r.mult(-this.k*((1/r.mag()) - (1/this.lij)));
                 F.add(r);
             }
@@ -43,9 +71,22 @@ class Boid {
         return F;
     }
 
-    Fg(v) {
-        let F = v;
+    Fg() {
+        let F = createVector();
+        F.add(this.vd);
         F.mult(this.wg);
+        // if(this.index < 1) {
+        //     print(F.x + ', ' + F.y);
+        // }
+        // let Fo = createVector();
+        // Fo.add(F);
+        // Fo.normalize();
+        // Fo.mult(10);
+        // push(); // draw a line to show where the Force is heading
+        //     strokeWeight(1);
+        //     stroke(255);
+        //     line(this.position.x, this.position.y, this.position.x + Fo.x, this.position.y + Fo.y);
+        // pop();
         return F;
     }
 
@@ -54,32 +95,83 @@ class Boid {
         for (let o of obs) {
             let d = dist(this.position.x, this.position.y,
                 o.position.x, o.position.y);
+            // print('|rio| <= rsens : ' + (r <= this.sensing)); false? why?
+            // if(r <= this.sensing) {
             if (d - o.radius <= this.sensing) {
+                // if (d - o.radius <= this.size/2) {
+                //     this.color = color(255,0,0);
+                // } else {
+                //     this.color = color(0,255,0);
+                // }
+                // let r = this.rij(this.position, o.position);
+                // let exceed1 = createVector(cos(r.heading())*o.radius, sin(r.heading())*o.radius);
+                // let exceed2 = createVector(cos(r.heading())*this.size/2, sin(r.heading())*this.size/2);
+                // push();
+                //     strokeWeight(1);
+                //     stroke(0,0,255);
+                //     line(this.position.x, this.position.y, o.position.x - exceed1.x, o.position.y - exceed1.y);
+                //     stroke(255,0,0);
+                //     line(o.position.x, o.position.y, o.position.x - exceed1.x, o.position.y - exceed1.y);
+                //     line(o.position.x, o.position.y, o.position.x - exceed1.x, o.position.y);
+                //     line(o.position.x - exceed1.x, o.position.y, o.position.x - exceed1.x, o.position.y - exceed1.y);
+                //     stroke(0,255,0);
+                //     line(this.position.x, this.position.y, this.position.x + exceed2.x, this.position.y + exceed2.y);
+                //     line(this.position.x, this.position.y + exceed2.y, this.position.x + exceed2.x, this.position.y + exceed2.y);
+                //     line(this.position.x, this.position.y, this.position.x, this.position.y + exceed2.y);
+                // pop();
+                // r.sub(exceed1);
+                // r.sub(exceed2);
                 checked.push(o);
             }
+            // else {
+            //     this.color = color(255,255,255);
+            // }
         }
         return checked;
     }
 
     Fobs(obs) {
         let F = createVector();
+        F.mult(0);
         let sigmaobs = this.sensing*sqrt(2);
         for (let o of obs) {
             let r = this.rij(this.position, o.position);
-            let newr = createVector(o.radius*cos(r.heading()), o.radius*sin(r.heading()));
-            r.sub(newr);
+            let exceed1 = createVector(cos(r.heading())*o.radius, sin(r.heading())*o.radius);
+            let exceed2 = createVector(cos(r.heading())*this.size/2, sin(r.heading())*this.size/2);
+            // push();
+            //     strokeWeight(1);
+            //     stroke(255);
+            //     line(this.position.x, this.position.y, this.position.x + Fo.x, this.position.y + Fo.y); // draw a line to show where the Force is heading
+            //     line(this.position.x, this.position.y, this.position.x + Fo.x, this.position.y + Fo.y); // draw a line to show where the Force is heading
+            // pop();
+            // // to obtain the actual distance between boid and obstacle
+            r.sub(exceed1); // substract with obstacle radius
+            r.sub(exceed2); // substract with boid radius
             r.mult(o.Eobs*(pow(sigmaobs/r.mag(),2*o.alphaLJ) - 2*pow(sigmaobs/r.mag(),o.alphaLJ))); // re-check the formula
             F.add(r);
         }
+        // let Fo = createVector();
+        // Fo.add(F);
+        // Fo.limit(20);
+        // push(); // draw a line to show where the Force is heading
+        //     strokeWeight(1);
+        //     stroke(255,0,0);
+        //     line(this.position.x, this.position.y, this.position.x - Fo.x, this.position.y - Fo.y);
+        // pop();
         return F;
     }
 
     Ftotal(boids,obs) {
-        let vd = createVector(1,0);
         let F = createVector();
+        F.mult(0);
         F.add(this.Fi(boids));
-        F.add(this.Fg(vd));
+        F.add(this.Fg(this.vd));
         F.sub(this.Fobs(obs));
+        push(); // draw a line to show where the Force is heading
+            strokeWeight(1);
+            stroke(255,0,0);
+            line(this.position.x, this.position.y, this.position.x + F.x, this.position.y + F.y);
+        pop();
         return F; // returns a vector number
     }
 
@@ -114,38 +206,40 @@ class Boid {
         return ds;
     }
 
-    // update(x, y) { // use this to moves the boids/individuals based on mouse position
-    //     if(x > width) {x = width;}
-    //     if(y > height) {y = height;}
-    //     this.position.x = x;
-    //     this.position.y = y;
-    update(boids,obs) {
-        // this.acceleration.add(this.Ftotal(boids,obs));
-        // this.velocity.add(this.acceleration);
-        // this.heading.add(this.velocity);
-        this.heading.add(this.vy(this.Ftotal(boids,obs)));
-        this.heading.normalize();
-        this.heading.mult(20);
-        // this.velocity.limit(this.maxSpeed);
-        this.velocity.x += this.vx(this.Ftotal(boids,obs));
-        this.velocity.y += this.vy(this.Ftotal(boids,obs));
-        this.position.add(this.velocity);
-        // this.acceleration.mult(0);
-        this.velocity.mult(0);
+    update(x, y) { // use this to moves the boids/individuals based on mouse position
+        if(x > width) {x = width;}
+        if(y > height) {y = height;}
+        this.position.x = x;
+        this.position.y = y;
+    // update(boids,obs) {
+    //     // this.acceleration.add(this.Ftotal(boids,obs));
+    //     // this.velocity.add(this.acceleration);
+    //     // this.heading.add(this.velocity);
+    //     this.heading.add(this.vy(this.Ftotal(boids,obs)));
+    //     this.heading.normalize();
+    //     this.heading.mult(20);
+    //     this.velocity.add(this.Ftotal(boids,obs));
+    //     this.velocity.limit(this.maxSpeed);
+    //     this.position.add(this.velocity);
+    //     // this.acceleration.mult(0);
+    //     this.velocity.mult(0);
     }
 
     show() {
         strokeWeight(this.size);
-        if (this.change) {
-            stroke(0,255,0);
-        } else {
-            stroke(255);
-        }
+        // if (this.change) {
+        //     stroke(0,255,0);
+        // } else {
+        //     stroke(255);
+        // }
+        stroke(this.color);
         point(this.position.x, this.position.y);
-        // strokeWeight(1);
-        // stroke(0,0,255);
-        // line(this.position.x, this.position.y, this.position.x + this.heading.x, this.position.y + this.heading.y); // orientation
-        // noFill();
-        // ellipse(this.position.x, this.position.y, this.sensing*2); // sensing radius
+        push();
+            strokeWeight(1);
+            stroke(0,0,255);
+            line(this.position.x, this.position.y, this.position.x + this.heading.x, this.position.y + this.heading.y); // orientation
+            noFill();
+            ellipse(this.position.x, this.position.y, this.sensing*2); // sensing radius
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,12 +1,33 @@
 const boids = [];
 const obstacles = [];
-const num = 15;
+const num = 1;
 let aveY = 0;
 // let imgnum = 1;
 // let t = 0;
+let table;
+
+function dataTable() {
+    table = new p5.Table();
+    table.addColumn('i'); // boids index
+    table.addColumn('x');
+    table.addColumn('y');
+    table.addColumn('theta');
+    table.addColumn('Fg.x');
+    table.addColumn('Fg.y');
+    table.addColumn('Fi.x');
+    table.addColumn('Fi.y');
+    table.addColumn('Fobs.x');
+    table.addColumn('Fobs.y');
+    table.addColumn('Ftot.x');
+    table.addColumn('Ftot.y');
+    table.addColumn('v.x');
+    table.addColumn('v.y');
+    table.addColumn('ds'); // coherency
+}
 
 function setup() {
     createCanvas(600, 400);
+    dataTable();
     for (let i = 0; i < num; i++) {
     	boids.push(new Boid(i));
         // while(boids[i].overlapPos(boids)) { // changing boids position so its not overlapping each other
@@ -17,7 +38,14 @@ function setup() {
         aveY += boids[i].position.y;
     }
     // Obstacle(x, y, radius, alphaLJ, Eobs, color)
-    obstacles.push(new Obstacle(width*(5/8), aveY/num, 10, 0.975, 0.01, 150));
+    for (let i = 0; i < 5; i++) {
+        if(i < 1) {
+            obstacles.push(new Obstacle(width*(3/8), aveY/num, 10, 0.975, 0.01, 150));
+        } else {
+            obstacles.push(new Obstacle(obstacles[i-1].position.x + 75, aveY/num, 10, 0.975, 0.01, 150));
+        }
+    }
+    // obstacles.push(new Obstacle(width*(5/8), aveY/num, 10, 0.975, 0.01, 150));
     // obstacles.push(new Obstacle(width*(5/8) + 50, aveY/num + 30, 10, 0.975, 0.01, 150));
     // obstacles.push(new Obstacle(width*(5/8) + 50, aveY/num - 30, 10, 0.975, 0.01, 150));
     // obstacles.push(new Obstacle(width*(5/8) + 120, aveY/num, 10, 0.975, 0.01, 150));
@@ -25,36 +53,63 @@ function setup() {
 
 function draw() {
     background(50);
-    let boidlast = boids[0]; // checking who's the last
-    let last = 0;
-    for (let i = 1; i < boids.length; i++ ) {
-        if( boids[i].position.x < boidlast.position.x ) {
-            boidlast = boids[i];
-            last = i;
-        }
-    }
-    boids[last].change = true;
+    // let boidlast = boids[0]; // checking who's the last
+    // let last = 0;
+    // for (let i = 1; i < boids.length; i++ ) {
+    //     if( boids[i].position.x < boidlast.position.x ) {
+    //         boidlast = boids[i];
+    //         last = i;
+    //     }
+    // }
+    // boids[last].change = true;
     for (let obs of obstacles) {
         obs.show();
     }
     for (let boid of boids) {
-        // boid.update(mouseX, mouseY); // use this to moves the boids/individuals based on mouse position
-        // boid.check(obstacles);
-        boid.update(boids,boid.check(obstacles));
+        boid.update(mouseX, mouseY); // use this to moves the boids/individuals based on mouse position
+        boid.Ftotal(boids, boid.check(obstacles));
+        // let F = boid.Ftotal(boids,boid.check(obstacles));
+        // let newRow = table.addRow();
+        // newRow.setNum('i', boid.index + 1); // boids index
+        // newRow.setNum('x', boid.position.x);
+        // newRow.setNum('y', boid.position.y);
+        // newRow.setNum('theta', degrees(boid.thetai(F)));
+        // newRow.setNum('Fg.x', boid.Fg().x);
+        // newRow.setNum('Fg.y', boid.Fg().y);
+        // newRow.setNum('Fi.x', boid.Fi(boids).x);
+        // newRow.setNum('Fi.y', boid.Fi(boids).y);
+        // newRow.setNum('Fobs.x', boid.Fobs(boid.check(obstacles)).x);
+        // newRow.setNum('Fobs.y', boid.Fobs(boid.check(obstacles)).y);
+        // newRow.setNum('Ftot.x', F.x);
+        // newRow.setNum('Ftot.y', F.y);
+        // newRow.setNum('v.x', p5.Vector.add(boid.velocity, F).x);
+        // newRow.setNum('v.y', p5.Vector.add(boid.velocity, F).y);
+        // newRow.setNum('ds', boid.coherency(boids)); // coherency
+        // newRow.setNum('vy');
+        // F.normalize();
+        // F.mult(10);
+        // boid.update(boids,boid.check(obstacles));
         boid.show();
-        boid.change = false;
+        // push();
+        //     strokeWeight(1);
+        //     stroke(0,0,255);
+        //     line(boid.position.x, boid.position.y, boid.position.x + F.x, boid.position.y + F.y);
+        // pop();
+        // print(boid.equilibrium(boids));
+        // boid.change = false;
     }
     // noLoop();
-    if (boidlast.position.x > width + boidlast.lij + 5) {
-        print('Process terminated');
-        noLoop();
-    }
+    // if (boidlast.position.x > width + boidlast.lij + 5) {
+    //     print('Process terminated');
+    //     // saveTable(table, 'new.csv');
+    //     noLoop();
+    // }
     // } else {
-    //     savingimg();
+    //     saveimg();
     // }
 }
 
-function savingimg() { // saving img per frame
+function saveimg() { // saving img per frame
         let numimg = 1;
         if(imgnum.toString().length < 3) {
             if(imgnum.toString().length < 2) {
